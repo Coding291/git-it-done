9b74d091fec18573b4031c4f85dd6715287186c5
Muhammad Humayun <humayun@MacBook-Pro.home>
2022-05-23 01:01:16 -0400

    fetch

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var getUserRepos = function() {
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+ fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+    console.log(data);
+    });
+ })};
+  console.log("outside");
+  getUserRepos();
